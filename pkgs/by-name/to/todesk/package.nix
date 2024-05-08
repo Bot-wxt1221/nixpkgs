fffd397792ab292a233e9a9aa8a7825f9ec9c8e3
wxt <3264117476@qq.com>
2024-05-08 23:28:49 +0800

    Add todesk

--- pkgs/by-name/to/todesk/package.nix
+++ pkgs/by-name/to/todesk/package.nix
@@ -3,37 +3,89 @@
 , fetchurl
 , dpkg
 , autoPatchelfHook
+, makeWrapper
 , gzip
 , gnutar
+, nspr
+, kmod
+, systemdMinimal
+, glib
+, libX11
+, libXrandr
+, glibc
+, libdrm
+, libGL
+, libXcomposite
+, libXdamage
+, libXfixes
+, libXtst
+, nss
+, libXxf86vm
+, gtk3
+, gdk-pixbuf
+, pango
+, appindicator-sharp
 }:
+
 stdenv.mkDerivation rec {
   pname = "todesk";
   version = "4.7.2.0";
 
   src = fetchurl {
-    url = "https://dl.todesk.com/linux/todesk-v4.7.2.0-amd64.deb";
+    url = "https://github.com/Bot-wxt1221/Bot-wxt1221-NvChad/releases/download/temp/todesk-v4.7.2.0-amd64.deb";
     sha256 = "sha256-v7VpXXFVaKI99RpzUWfAc6eE7NHGJeFrNeUTbVuX+yg=";
   };
 
-  nativeBuildInputs = [ dpkg autoPatchelfHook];
+  nativeBuildInputs = [ dpkg autoPatchelfHook ];
+  buildInputs = [
+   nspr
+   kmod
+   systemdMinimal
+   glib
+   libX11
+   libXrandr
+   glibc
+   libdrm
+   libGL
+   libXcomposite
+   libXdamage
+   libXfixes
+   libXtst
+   nss
+   libXxf86vm
+   gtk3
+   gdk-pixbuf
+   pango
+   appindicator-sharp
+  ];
+
+  autoPatchelfIgnoreMissingDeps = [ "iHD_drv_video.so" "libglut.so" "libigdgmm.so" "libmfx.so" "libmfxhw64.so" "libva.so" "libva-drm.so" "libva-x11.so" "libzrtc.so" ];
 
   unpackPhase = ''
     runHook preUnpack
-    dpkg -x $src ./todesk
+
+    dpkg -x $src ./todesk-src
+
     runHook postUnpack
   '';
 
   installPhase = ''
     runHook preInstall
-    mkdir -pv $out
-    cp -r ./todesk/* "$out"
+
+    mkdir -p "$out"
+    mkdir -p $out/lib
+    cp -r todesk-src/* "$out"
 
     runHook postInstall
   '';
-
+  postInstall = ''
+    install -Dm644 $out/etc/systemd/system/todeskd.service $out/lib/systemd/system/todeskd.service
+    substituteInPlace $out/etc/systemd/system/todeskd.service \
+     --replace /opt/todesk/bin $out/opt/todesk/bin
+  '';
 
   meta = with lib; {
-    description = "Remote control and team work";
+    description = "A Remote Desktop Application";
     homepage = "https://www.todesk.com/linux.html";
     license = licenses.unfree;
     platforms = with platforms; [ "x86_64-linux" ];
@@ -42,4 +94,3 @@ stdenv.mkDerivation rec {
     mainProgram = "ToDesk";
   };
 }
-
