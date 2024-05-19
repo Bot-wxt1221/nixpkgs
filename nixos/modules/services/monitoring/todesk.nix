8b8ba62c3bdcd8c020f797bd5a9f8d594c2666ca
wxt <3264117476@qq.com>
2024-05-19 14:49:03 +0800

    Todesk

--- nixos/modules/services/monitoring/todesk.nix
+++ nixos/modules/services/monitoring/todesk.nix
@@ -14,7 +14,7 @@ in
  options = {
 
     services.todesk.enable = mkEnableOption "ToDesk daemon";
-    services.todesk.user = mkString "Todesk daemon user";
+    services.todesk.user = mkOption {description = "Todesk daemon user";type=types.string};
 
   };
 
