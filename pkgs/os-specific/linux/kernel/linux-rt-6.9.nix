c40d47b93b6d37e7045e35d1be653c7ed8c48411
wxt <3264117476@qq.com>
2024-05-27 21:06:26 +0800

    6.9.2

--- pkgs/os-specific/linux/kernel/linux-rt-6.9.nix
+++ pkgs/os-specific/linux/kernel/linux-rt-6.9.nix
@@ -6,7 +6,7 @@
 , ... } @ args:
 
 let
-  version = "6.9.1-rt5"; # updated by ./update-rt.sh
+  version = "6.9.2-rt5"; # updated by ./update-rt.sh
   branch = lib.versions.majorMinor version;
   kversion = builtins.elemAt (lib.splitString "-" version) 0;
 in buildLinux (args // {
@@ -17,7 +17,7 @@ in buildLinux (args // {
     else lib.replaceStrings ["-"] [".0-"] version;
 
   src = fetchurl {
-    url = "https://github.com/Bot-wxt1221/linux/archive/refs/tags/6.9.1.tar.gz";
+    url = "https://github.com/Bot-wxt1221/linux/archive/refs/tags/6.9.2-rt5.tar.gz";
     sha256 = "sha256-yPct/UAlVP01ut3yvf5du5mwK3fpK9DQ9BBX5YmjH+Y=";
   };
 
