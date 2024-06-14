c7c78529c1faf1cb06d438bebc52f7b56244f643
wxt <3264117476@qq.com>
2024-06-14 20:26:20 +0800

    2024.6.14

--- pkgs/by-name/to/todesk/package.nix
+++ pkgs/by-name/to/todesk/package.nix
@@ -1,6 +1,6 @@
 { stdenv
 , lib
-, requireFile
+, fetchurl
 , dpkg
 , autoPatchelfHook
 , makeWrapper
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
   pname = "todesk";
   version = "4.7.2.0";
 
-  src = requireFile {
+  src = fetchurl {
     url = "https://dl.todesk.com/linux/todesk-v${version}-amd64.deb";
     sha256 = "sha256-v7VpXXFVaKI99RpzUWfAc6eE7NHGJeFrNeUTbVuX+yg=";
     curlOptsList = [ "--user-agent" "Mozilla/5.0" ]; 
