c6112427736a1657b7bcb115f8fca163f2ec2d2b
wxt <3264117476@qq.com>
2024-10-03 17:16:48 +0800

    jen: fix build

--- pkgs/by-name/je/jen/package.nix
+++ pkgs/by-name/je/jen/package.nix
@@ -16,10 +16,14 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-nouAHEo5JJtZ0pV8ig/iJ3eB8uPz3yMVIYP6RrNVlSA=";
   };
 
-  cargoHash = "sha256-Nia5SsmxdBrWO4zmkoi0gCmknkhwsM8PZ52aPVMnp90=";
+  cargoHash = "sha256-Y81YqrzJSar0BxhQb7Vm/cZ9E6krlyZesXPY+j37IHA=";
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
     darwin.apple_sdk.frameworks.Security
+  ];
+
+  cargoPatches = [
+    ./0001-update-time.patch
   ];
 
   passthru.updateScript = nix-update-script { };
