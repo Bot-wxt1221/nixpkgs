a83adb944e713eaf5152e88da05b9fa83935b1a4
wxt <3264117476@qq.com>
2024-10-04 21:37:23 +0800

    tt

--- pkgs/by-name/ic/icpp/package.nix
+++ pkgs/by-name/ic/icpp/package.nix
@@ -1,12 +1,18 @@
 {
-  stdenv,
+  clangStdenv,
   lib,
   fetchFromGitHub,
   cmake,
-  python3
+  python3,
+  valgrind,
+  pkg-config,
+  libxml2,
+  libedit,
+  libunwind,
+  ocaml,
 }:
 
-stdenv.mkDerivation(finalAttrs:{
+clangStdenv.mkDerivation(finalAttrs:{
   pname = "icpp";
   version = "0.1.2";
 
@@ -14,12 +20,26 @@ stdenv.mkDerivation(finalAttrs:{
     owner = "vpand";
     repo = "icpp";
     rev = "refs/tags/v${finalAttrs.version}";
-    hash = "";
+    hash = "sha256-eAWZZdYveIxIBIdyuRKf2LjtC84mWq3FeQsQHTp35GA=";
     fetchSubmodules = true;
   };
 
+  preConfigure = ''
+    cmake -B clangconf -DCMAKE_BUILD_TYPE=Release ./cmake/clangconf
+    cmake --build clangconf -- clang runtimes -j8
+  '';
+
   nativeBuildInputs = [
     cmake
+    pkg-config
+    ocaml
     python3
+  ];
+
+  buildInputs = [
+    valgrind
+    libunwind
+    libedit
+    libxml2
   ];
 })
