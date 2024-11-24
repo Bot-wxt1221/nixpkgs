0f2273af01f8460b5fd9794b0b221602c8b368a6
wxt <3264117476@qq.com>
2024-11-24 21:39:10 +0800

    tt

--- pkgs/by-name/ne/nearcore/package.nix
+++ pkgs/by-name/ne/nearcore/package.nix
@@ -1,37 +1,40 @@
-{ rustPlatform, lib, fetchFromGitHub
-, zlib, openssl
-, pkg-config, protobuf
+{
+  rustPlatform,
+  lib,
+  fetchFromGitHub,
+  zlib,
+  openssl,
+  pkg-config,
+  protobuf,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nearcore";
-  version = "1.30.1";
+  version = "2.3.1";
 
   # https://github.com/near/nearcore/tags
   src = fetchFromGitHub {
     owner = "near";
     repo = "nearcore";
-    # there is also a branch for this version number, so we need to be explicit
     rev = "refs/tags/${version}";
-
-    sha256 = "sha256-VjvHCiWjsx5Y7xxqck/O9gSNrL8mxCTosLwLqC85ywY=";
+    hash = "sha256-U8SjyhjcuaecIEH7uyKcjXB0g5NnEq64gcbbedZRZmI=";
   };
 
-  cargoHash = "sha256-5Gs1sAzjuUO3IkwMX1NeA/Sbax0qtwvulyT66AQaNjs=";
-  cargoPatches = [ ./0001-make-near-test-contracts-optional.patch ];
+  useFetchCargoVendor = true;
+  cargoHash = "";
 
   postPatch = ''
     substituteInPlace neard/build.rs \
       --replace 'get_git_version()?' '"nix:${version}"'
   '';
-
-  CARGO_PROFILE_RELEASE_CODEGEN_UNITS = "1";
-  CARGO_PROFILE_RELEASE_LTO = "fat";
-  NEAR_RELEASE_BUILD = "release";
-
-  OPENSSL_NO_VENDOR = 1; # we want to link to OpenSSL provided by Nix
+  env = {
+    CARGO_PROFILE_RELEASE_CODEGEN_UNITS = "1";
+    CARGO_PROFILE_RELEASE_LTO = "fat";
+    NEAR_RELEASE_BUILD = "release";
+    OPENSSL_NO_VENDOR = 1; # we want to link to OpenSSL provided by Nix
+  };
 
   # don't build SDK samples that require wasm-enabled rust
-  buildAndTestSubdir = "neard";
+#  buildAndTestSubdir = "neard";
   doCheck = false; # needs network
 
   buildInputs = [
@@ -48,13 +51,12 @@ rustPlatform.buildRustPackage rec {
   # fat LTO requires ~3.4GB RAM
   requiredSystemFeatures = [ "big-parallel" ];
 
-  meta = with lib; {
+  meta = {
     description = "Reference client for NEAR Protocol";
     homepage = "https://github.com/near/nearcore";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ mikroskeem ];
-    # only x86_64 is supported in nearcore because of sse4+ support, macOS might
-    # be also possible
-    platforms = [ "x86_64-linux" ];
+    license = lib.licenses.gpl3;
+    mainProgram = "";
+    maintainers = with lib.maintainers; [ mikroskeem ];
+    platforms = lib.platforms.darwin ++ lib.platforms.linux;
   };
 }
