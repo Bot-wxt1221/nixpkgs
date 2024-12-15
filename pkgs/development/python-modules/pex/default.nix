b6ac91d9da3c13a4df959de616aa490f3993f46a
wxt <3264117476@qq.com>
2024-12-15 21:15:39 +0800

    python312Packages.pex: fix build

--- pkgs/development/python-modules/pex/default.nix
+++ pkgs/development/python-modules/pex/default.nix
@@ -2,35 +2,66 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  hatchling,
+  setuptools,
+  pytest,
   pythonOlder,
+  pytest-xdist,
+  coloredlogs,
+  ansicolors,
+  psutil,
+  pexpect,
+  pycodestyle,
 }:
 
 buildPythonPackage rec {
   pname = "pex";
-  version = "2.20.1";
+  version = "2.27.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-JvZiVb/qzkILBaALrCU2rldUgSgSD1eo4AqMPgEdJ50=";
+    hash = "sha256-tUqt4u9tkYAyVBjHw0ABuWbBSlo28FHEGGWPoVy7Vk4=";
   };
 
-  build-system = [ hatchling ];
+  dependencies = [
+    ansicolors
+    coloredlogs
+    pexpect
+    psutil
+  ];
 
-  # A few more dependencies I don't want to handle right now...
-  doCheck = false;
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [
+    pycodestyle
+    pytest
+    pytest-xdist
+  ];
+
+  postPatch = ''
+    substituteInPlace testing/bin/run_tests.py \
+      --replace-fail 'subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode("ascii").strip()' "\"$PWD\"" \
+      --replace-fail '"pytest"' '"pytest","--deselect=test_present_non_empty_namespace_packages_metadata_does_warn"'  # Need twitter-common-lang"
+  '';
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+
+    python testing/bin/run_tests.py -vss
+
+    runHook postInstallCheck
+  '';
 
   pythonImportsCheck = [ "pex" ];
 
-  meta = with lib; {
+  meta = {
     description = "Python library and tool for generating .pex (Python EXecutable) files";
     homepage = "https://github.com/pantsbuild/pex";
     changelog = "https://github.com/pantsbuild/pex/releases/tag/v${version}";
-    license = licenses.asl20;
-    maintainers = with maintainers; [
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
       copumpkin
       phaer
     ];
